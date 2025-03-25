7793cf3b30a53210cdaecb3c5cf795550acaeddb
playmean <im.playmean@gmail.com>
2025-03-25 15:29:32 +0300

    chore: release bump fixed

--- release.sh
+++ release.sh
@@ -2,6 +2,6 @@
 
 VERSION=`npx semverity bump --tidy`
 
-npx semverity patch --files package.json:version package-lock.json:version,packages..version manifest.json:version --tidy --commit bump $VERSION
+npx semverity patch --files package.json:version package-lock.json:version,packages..version manifest.json:version --tidy --commit bump
 
 git tag $VERSION && git push origin $VERSION
