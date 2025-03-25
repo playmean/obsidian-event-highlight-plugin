1785458132fe808601b5b73e5d634ded2cfba1af
Aleksey Korolev <im.playmean@gmail.com>
2025-03-25 15:22:27 +0300

    chore: short release version

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-VERSION=`npx semverity bump`
+VERSION=`npx semverity bump --tidy`
 
-npx semverity patch --files package.json:version package-lock.json:version,packages..version manifest.json:version --commit bump $VERSION
+npx semverity patch --files package.json:version package-lock.json:version,packages..version manifest.json:version --tidy --commit bump $VERSION
 
 git tag $VERSION && git push origin $VERSION
